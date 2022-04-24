a9b69a612e98a113e580e63f36213a7712cfc270
ferhatclk <59114579+ferhatclk@users.noreply.github.com>
2022-04-24 17:40:46 +0300

    homework 3 added

---
+++ homework3.sql
@@ -0,0 +1,18 @@
+-- Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+-- 1. country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+
+SELECT country FROM country WHERE country LIKE 'A%a';
+
+-- 2. country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+
+SELECT country FROM country WHERE country LIKE '%_____n';
+
+-- 3. film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+
+SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
+
+-- 4. film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+
+SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
+
